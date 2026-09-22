31c230634203afdbb948f65bd4523aad401df0ff
Yasin Hoca <myasinozsaglam@gmail.com>
2026-09-22 15:04:07 +0300

    Add files via upload

---
+++ 00_hosgeldin.py
@@ -0,0 +1,14 @@
+print("Python'a hoşgeldiniz")
+isim = "caner"
+soyisim = "güngör"
+print("Hoşgeldin",isim,soyisim)
+
+n = input("İsminizi girimiz")
+print("Holgeldin",n)
+
+
+a = 5
+b = 8
+c = a + b
+print("a + b =",c)
+print(a,"+",b,"=",c)
